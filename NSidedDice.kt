f02896b53f8ec2c8225685a38d002e7f878ff404
yashgoyal97 <60594329+yashgoyal97@users.noreply.github.com>
2021-02-09 23:31:39 +0530

    Create NSidedDice.kt
    - Displaying the concept of class and object
    - Dynamically created n sided dice

---
+++ NSidedDice.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val myFirstDice = Dice(6)
+    val diceRoll = myFirstDice.roll()
+    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+    
+    val mySecondDice = Dice(20)
+    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
+}
+
+class Dice(val numSides: Int) {
+
+    fun roll(): Int {
+        val randomNumber = (1..numSides).random()
+        return randomNumber
+    }
+}

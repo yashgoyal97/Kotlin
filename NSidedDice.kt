3a5bf57d7bf4c46cbaf3bcead8b1b2357160c4c8
yashgoyal97 <60594329+yashgoyal97@users.noreply.github.com>
2021-02-09 23:48:33 +0530

    added color to dice class

--- NSidedDice.kt
+++ NSidedDice.kt
@@ -1,13 +1,13 @@
 fun main() {
-    val myFirstDice = Dice(6)
+    val myFirstDice = Dice(6,"Green")
     val diceRoll = myFirstDice.roll()
-    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+    println("Your ${myFirstDice.numSides} sided, ${myFirstDice.color} color dice rolled ${diceRoll}!")
     
-    val mySecondDice = Dice(20)
-    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
+    val mySecondDice = Dice(20,"Red")
+    println("Your ${mySecondDice.numSides} sided, ${mySecondDice.color} color dice rolled ${mySecondDice.roll()}!")
 }
 
-class Dice(val numSides: Int) {
+class Dice(val numSides: Int, val color: String) {
 
     fun roll(): Int {
         return (1..numSides).random()
